252f7f82dfa572a09df1a73c691efd14a86071e1
jlelz <79492325+jlelz@users.noreply.github.com>
2024-12-28 15:51:42 -0700

    bookend textures

--- App.lua
+++ App.lua
@@ -600,6 +600,7 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
             self.Heading.BookEnd:SetSize( 65,self.Heading:GetHeight() );
             self.Heading.BookEnd:SetVertTile( true );
             self.Heading.BookEnd:SetPoint( 'topright',self.Heading,'topright',0,5 );
+            self.Heading.BookEnd:SetAlpha( .5 );
 
             self.FilterBox = CreateFrame( 'EditBox',self.Name..'Filter',self.Config,'SearchBoxTemplate' );
             self.FilterBox:SetPoint( 'topleft',self.Heading,'topleft',15,( ( self.Heading:GetHeight() )*-1 )+25 );
@@ -680,6 +681,7 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
             self.Browser.BookEnd:SetTexture( 'Interface\\azerite\\azeritecenterbggold' );
             self.Browser.BookEnd:SetSize( 65,self.Browser:GetHeight() );
             self.Browser.BookEnd:SetPoint( 'topright',self.Browser,'topright',0,0 );
+            self.Browser.BookEnd:SetAlpha( .5 );
 
             self.ScrollFrame = CreateFrame( 'ScrollFrame',self.Name..'ScrollFrame',self.Browser,'UIPanelScrollFrameTemplate' );
             self.ScrollFrame:SetAllPoints( self.Browser );
@@ -699,6 +701,13 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
 
             self.Footer.Art = Addon.FRAMES:AddBackGround( self.Footer );
             self.Footer.Art:SetAllPoints( self.Footer );
+
+            self.Footer.BookEnd = self.Browser:CreateTexture( nil,'ARTWORK',nil,3 );
+            self.Footer.BookEnd:SetTexture( 'Interface\\azerite\\azeritecenterbggold' );
+            self.Footer.BookEnd:SetSize( 65,self.Footer:GetHeight() );
+            self.Footer.BookEnd:SetVertTile( true );
+            self.Footer.BookEnd:SetPoint( 'topright',self.Footer,'topright',0,0 );
+            self.Footer.BookEnd:SetAlpha( .5 );
 
             self.Stats = CreateFrame( 'Frame',self.Name..'FooterStats',self.Footer );
             self.Stats:SetSize( self.Footer:GetWidth()/2,self.Footer:GetHeight() );
